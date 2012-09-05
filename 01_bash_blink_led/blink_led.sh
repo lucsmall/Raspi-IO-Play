52dee139690bb0d18874306ac23f04e745409017
Luc Small <luc.small@gmail.com>
2012-09-05 00:12:35 +0000

    Initial import

---
+++ 01_bash_blink_led/blink_led.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Use the "gpio" program that comes as part of the Wiring Pi package
+# to blink a let 5 times
+#
+
+# Set the output according to the Broadcom GPIO numbering scheme
+OUTPUT0=18
+
+echo set GPIO$OUTPUT0 as output
+gpio -g mode $OUTPUT0 out
+
+for ((i=1; i <= 5; i++))
+do
+	echo set GPIO$OUTPUT0 high
+	gpio -g write $OUTPUT0 1
+	sleep 1
+	echo set GPIO$OUTPUT0 low
+	gpio -g write $OUTPUT0 0
+	sleep 1
+done
+
+echo set GPIO$OUTPUT0 as input
+gpio -g mode $OUTPUT0 in
